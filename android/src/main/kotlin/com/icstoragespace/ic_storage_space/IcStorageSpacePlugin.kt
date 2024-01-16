f97718d8168c23bbb2d15eac7955df73663a7797
leeyi <liyuanbing@yongdaxing.cn>
2024-01-16 17:33:33 +0800

    A more accurate way to get Android storage space( TODO: api compatibility is subject to rigorous testing （minAPI is 26?) )

--- android/src/main/kotlin/com/icstoragespace/ic_storage_space/IcStorageSpacePlugin.kt
+++ android/src/main/kotlin/com/icstoragespace/ic_storage_space/IcStorageSpacePlugin.kt
@@ -11,6 +11,13 @@ import io.flutter.plugin.common.PluginRegistry.Registrar
 import android.os.Environment
 import android.os.StatFs
 
+import android.content.Context
+import android.os.storage.StorageManager
+import android.app.usage.StorageStats
+import android.app.usage.StorageStatsManager
+import java.io.IOException
+import java.util.UUID
+
 /** IcStorageSpacePlugin */
 class IcStorageSpacePlugin : FlutterPlugin, MethodCallHandler {
     /// The MethodChannel that will the communication between Flutter and native Android
@@ -18,8 +25,10 @@ class IcStorageSpacePlugin : FlutterPlugin, MethodCallHandler {
     /// This local reference serves to register the plugin with the Flutter Engine and unregister it
     /// when the Flutter Engine is detached from the Activity
     private lateinit var channel: MethodChannel
+    private lateinit var context: Context
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        context = flutterPluginBinding.getApplicationContext()
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "ic_storage_space")
         channel.setMethodCallHandler(this)
     }
@@ -28,7 +37,7 @@ class IcStorageSpacePlugin : FlutterPlugin, MethodCallHandler {
         when (call.method) {
             "getPlatformVersion" -> result.success("Android ${android.os.Build.VERSION.RELEASE}")
             "getFreeDiskSpaceInBytes" -> result.success(getFreeDiskSpaceInBytes())
-            "getTotalDiskSpaceInBytes" -> result.success(getTotalDiskSpaceInBytes())
+            "getTotalDiskSpaceInBytes" -> result.success( getTotalDiskSpaceInBytes())
             else -> result.notImplemented()
         }
     }
@@ -47,14 +56,20 @@ class IcStorageSpacePlugin : FlutterPlugin, MethodCallHandler {
             return stat.blockSize.toLong() * stat.availableBlocks.toLong()
     }
 
+    /**
+     * A more accurate way to get Android storage space
+     *  TODO: api compatibility is subject to rigorous testing （minAPI is 26?)
+     *  https://gist.github.com/li-jkwok/e460a042326e8509ada9ec23ae677bdf
+     */
     private fun getTotalDiskSpaceInBytes(): Long {
-        val stat = StatFs(Environment.getExternalStorageDirectory().path)
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2)
-            return stat.blockSizeLong * stat.blockCountLong
-        else
-            return stat.blockSize.toLong() * stat.blockCount.toLong()
+        val storageManager = context.getSystemService(Context.STORAGE_SERVICE) as StorageManager
+        val storageStatsManager = context.getSystemService(Context.STORAGE_STATS_SERVICE) as StorageStatsManager
+        val storageVolumes = storageManager.storageVolumes
+        var totalBytes = 0L
+        for (volume in storageVolumes) {
+            val uuid = volume.uuid?.let { UUID.fromString(it) } ?: StorageManager.UUID_DEFAULT
+            totalBytes += storageStatsManager.getTotalBytes(uuid)
+        }
+        return totalBytes.toLong()
     }
-
-
 }
