3b2a585e434e2edcb12c47fa7beb563a5bb2487d
leeyi <liyuanbing@yongdaxing.cn>
2024-01-26 09:30:52 +0800

    fix deprecated function setMockMethodCallHandler

--- test/ic_storage_space_test.dart
+++ test/ic_storage_space_test.dart
@@ -8,13 +8,15 @@ void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, (MethodCall methodCall) async {
       return '42';
     });
   });
 
   tearDown(() {
-    channel.setMockMethodCallHandler(null);
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMessageHandler(channel.name, null);
   });
 
   test('getPlatformVersion', () async {
